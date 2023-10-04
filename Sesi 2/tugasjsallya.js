ea9441b664e918e4c0f595d97c68eb255c73a81f
allyasaf592 <115513872+allyasaf592@users.noreply.github.com>
2023-10-04 14:15:44 +0700

    Add files via upload

---
+++ Sesi 2/tugasjsallya.js
@@ -0,0 +1,8 @@
+let firstName = "Allya";
+let lastName = "Saffira";
+let age = 21;
+let isMarried = false;
+
+console.log(`Nama Lengkap : ${firstName} ${lastName}`);
+console.log(`Umur : ${age}`);
+console.log(`Is Married : ${isMarried}`);
